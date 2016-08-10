74f40a4b57abcfa81af69f9d077168160e198826
AlexOlivieri <alex.olivieri81@gmail.com>
2016-08-10 15:34:24 +0200

    Init Android project

---
+++ app/src/main/java/ch/hevs/datasemlab/cityzen/TemporalActivity.java
@@ -0,0 +1,13 @@
+package ch.hevs.datasemlab.cityzen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemporalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temporal);
+    }
+}
