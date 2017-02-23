e717f75250c9919528a5bc5c8b4ee155cb1f2c5a
AlexOlivieri <alex.olivieri81@gmail.com>
2017-02-23 10:54:20 +0100

    Final App For the Demo - Rating bar added

--- app/src/main/java/ch/hevs/datasemlab/cityzen/CulturalInterestsAdapter.java
+++ app/src/main/java/ch/hevs/datasemlab/cityzen/CulturalInterestsAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
@@ -23,6 +24,7 @@ public class CulturalInterestsAdapter extends CursorAdapter{
     private LayoutInflater cursorInflater;
 
     ImageView imageView;
+    RatingBar ratingBar;
 
     public CulturalInterestsAdapter(Context context, Cursor cursor, int flags){
         super(context, cursor, 0);
@@ -41,13 +43,13 @@ public class CulturalInterestsAdapter extends CursorAdapter{
     public void bindView(View view, Context context, Cursor cursor){
         imageView = (ImageView) view.findViewById(R.id.image_view_cultural_interest_image);
         TextView textViewTitle = (TextView) view.findViewById(R.id.text_view_cultural_item_title);
-        //TextView textViewDescription = (TextView) view.findViewById(R.id.text_view_cultural_item_description);
+
+        ratingBar = (RatingBar) view.findViewById(R.id.rating_bar_2);
+        ratingBar.setFocusable(false);
 
         String title = cursor.getString(cursor.getColumnIndex("Title"));
-        //String description = cursor.getString(cursor.getColumnIndex("Description"));
 
         byte[] imageByte = cursor.getBlob(cursor.getColumnIndex("Image"));
-        //new ImageDownloader().execute(image);
 
         //TODO:  get image with handler
 
@@ -61,7 +63,6 @@ public class CulturalInterestsAdapter extends CursorAdapter{
         Bitmap image = BitmapFactory.decodeByteArray(imageByte, 0, imageByte.length);
         imageView.setImageBitmap(Bitmap.createScaledBitmap(image, 200, 200, false));
         textViewTitle.setText(title);
-        //textViewDescription.setText(description);
     }
 
 }
